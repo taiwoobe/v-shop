e97ee406d4b7a99d21bf38f16f981d04ea131bfe
Taiwo <taiwo.ibidapoobe@gmail.com>
2019-10-09 14:23:06 +0100

    testing the mocked products API from Vuex

---
+++ src/components/ProductListComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="product in products" :key="product.id">{{ product.title }} - {{ product.price }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import shop from '@/api/shop.js';
+export default {
+    data() {
+        return {
+            products: []
+        }
+    },
+    created() {
+        shop.getProducts(products => {
+            this.products = products;
+        })
+    },
+}
+</script>
+
+<style lang="scss">
+    
+</style>
