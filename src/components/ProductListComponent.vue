9366b4c212be022ef1a83fe0a91159e2de278128
Taiwo <taiwo.ibidapoobe@gmail.com>
2019-10-18 12:06:24 +0100

    setting up the getters and setter methods for VueX

--- src/components/ProductListComponent.vue
+++ src/components/ProductListComponent.vue
@@ -1,27 +1,35 @@
 <template>
     <div>
-        <ul>
+        <img v-if="loading" src="https://i.imgur.com/JfPpwOA.gif" alt="Image Spinner">
+        <ul v-else>
             <li v-for="product in products" :key="product.id">{{ product.title }} - {{ product.price }}</li>
         </ul>
     </div>
 </template>
 
 <script>
-import shop from '@/api/shop.js';
+import store from '@/store/store.js';
 export default {
     data() {
         return {
-            products: []
+            // products: []
+            loading: false
+        }
+    },
+    computed: {
+        products() {
+            // return store.state.products;
+            return store.getters.availableProducts;
         }
     },
     created() {
-        shop.getProducts(products => {
-            this.products = products;
-        })
+        this.loading = true;
+        store.dispatch('fetchProdcts').then(() => {
+            this.loading = false;
+        });
     },
 }
 </script>
 
 <style lang="scss">
-    
 </style>
--- src/store/store.js
+++ src/store/store.js
@@ -1,16 +1,41 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import shop from '@/api/shop.js';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-
+  state: { // = data
+    products: []
   },
-  mutations: {
-
+  mutations: { // = setting & updating the state
+    setProducts(state, products) {
+      state.products = products;
+    }
   },
-  actions: {
-
+  getters: { // = computed properties
+    productsCount() {
+    },
+    availableProducts(state, getters) {
+      return state.products.filter(products => products.inventory > 0);
+    }
+  },
+  actions: { // = methods
+    fetchProdcts({commit}) {
+      return new Promise((resolve, reject) => {
+        shop.getProducts(products => {
+          commit('setProducts', products);
+          resolve()
+        })
+      })
+    },
+    addToCart(context, product) {
+      if (product.inventory > 0) {
+        context.commit('pushProductToCart', product);
+      } else {
+        // show out of stock message to the user
+        console.log('Product is out of stock');
+      }
+    }
   }
 })
